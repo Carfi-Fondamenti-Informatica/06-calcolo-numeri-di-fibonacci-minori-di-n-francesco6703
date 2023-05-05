4ad6259bedc5909ded06c3bb381e6aae4244e142
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-05-05 15:13:06 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
+	int n, x, a=1, b=0, fibonacci=1;
+	cin >> x;
+	cout << fibonacci << "\n";
+
+	while (fibonacci <=x)
+	{
+		fibonacci=a+b;
+		b=a;
+		a=fibonacci;
+		if (x>=fibonacci)
+		{
+			cout << fibonacci << "\n";
+		}
+	}
+
    return 0;
 }
